aff940ca006cbcb512b06e76d32624998fb6f0e5
wenxue11 <714876076@qq.com>
2019-06-18 10:26:26 +0800

    wx

--- src/views/pygl/pyfayxsgrjh/pyfacx.vue
+++ src/views/pygl/pyfayxsgrjh/pyfacx.vue
@@ -1,80 +1,97 @@
-<template >
+<template>
   <div class="app-container">
-    <div>
-      学生类型：
-      <el-select v-model="studetTypeCode"  style="width: 15%" filterable placeholder="请选择">
-        <el-option
-          v-for="item in list"
-          :key="item.companyName"
-          :label="item.companyName"
-          :value="item.companyName"
-        />
-      </el-select>
-      学院：
-      <el-select v-model="studetTypeCode"  style="width: 15%" filterable placeholder="请选择">
-        <el-option
-          v-for="item in list"
-          :key="item.companyName"
-          :label="item.companyName"
-          :value="item.companyName"
-        />
-      </el-select>
-      专业：
-      <el-select v-model="studetTypeCode"  style="width: 15%" filterable placeholder="请选择">
-        <el-option
-          v-for="item in list"
-          :key="item.companyName"
-          :label="item.companyName"
-          :value="item.companyName"
-        />
-      </el-select>
-
-    </div>
-    <div style="text-align: center;">
-        培养方案列表
-    </div>
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="序号"
-        width="80">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="培养方案名称"
-        width="550">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="所属专业"
-        width="230">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="起始时间"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="详细"
-      >
-      </el-table-column>
-    </el-table>
+    <!--<div>-->
+    <!--{{ $t('cultivatingSchemeQuery.studentType')}}:-->
+    <!--<el-select   style="width: 15%" filterable placeholder="请选择">-->
+    <!--<el-option-->
+    <!--&lt;!&ndash;v-for="item in list"&ndash;&gt;-->
+    <!--&lt;!&ndash;:key="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:label="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:value="item.companyName"&ndash;&gt;-->
+    <!--/>-->
+    <!--</el-select>-->
+    <!--{{ $t('cultivatingSchemeQuery.school') }}:-->
+    <!--<el-select   style="width: 15%" filterable placeholder="请选择">-->
+    <!--<el-option-->
+    <!--&lt;!&ndash;v-for="item in list"&ndash;&gt;-->
+    <!--&lt;!&ndash;:key="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:label="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:value="item.companyName"&ndash;&gt;-->
+    <!--/>-->
+    <!--</el-select>-->
+    <!--{{ $t('cultivatingSchemeQuery.major') }}:-->
+    <!--<el-select  style="width: 15%" filterable placeholder="请选择">-->
+    <!--<el-option-->
+    <!--&lt;!&ndash;v-for="item in list"&ndash;&gt;-->
+    <!--&lt;!&ndash;:key="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:label="item.companyName"&ndash;&gt;-->
+    <!--&lt;!&ndash;:value="item.companyName"&ndash;&gt;-->
+    <!--/>-->
+    <!--</el-select>-->
+    <!--{{ $t('cultivatingSchemeQuery.year') }}:-->
+    <!--<el-input  placeholder="请输入内容" style="width: 15%"></el-input>-->
+    <!--</div>-->
+    <!--<el-row>-->
+    <!--<el-col :span="24"><div class="grid-content bg-purple-light" style="text-align: center;">-->
+    <!--{{ $t('cultivatingSchemeQuery.ListOfCultivation') }}-->
+    <!--</div>-->
+    <!--</el-col>-->
+    <!--</el-row>-->
+    <!--<el-table-->
+    <!--:data="tablelist"-->
+    <!--border-->
+    <!--style="width: 100%">-->
+    <!--<el-table-column-->
+    <!--:label="$t('projectParticipation.number')"-->
+    <!--width="80">-->
+    <!--</el-table-column>-->
+    <!--<el-table-column-->
+    <!--:label="$t('cultivatingSchemeQuery.cultivatingSchemeName')"-->
+    <!--width="550">-->
+    <!--</el-table-column>-->
+    <!--<el-table-column-->
+    <!--:label="$t('cultivatingSchemeQuery.major1')"-->
+    <!--width="230">-->
+    <!--</el-table-column>-->
+    <!--<el-table-column-->
+    <!--:label="$t('cultivatingSchemeQuery.startTime')"-->
+    <!--width="180">-->
+    <!--</el-table-column>-->
+    <!--<el-table-column-->
+    <!--:label="$t('cultivatingSchemeQuery.detail')"-->
+    <!--&gt;-->
+    <!--</el-table-column>-->
+    <!--</el-table>-->
   </div>
 </template>
 <script>
+import { cultivateSchemeShow } from '@/api/user'
 export default {
   data() {
     return {
-      tableData: []
+      tablelist: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      cultivateSchemeShow().then(res => {
+        console.log(res)
+        this.tablelist = res.data
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 </style>
