74271576d2df616ed786c493cb75859c5abe41ff
wenxue11 <714876076@qq.com>
2019-06-24 16:55:13 +0800

    wx

--- src/views/pygl/pyfayxsgrjh/pyfacx.vue
+++ src/views/pygl/pyfayxsgrjh/pyfacx.vue
@@ -3,30 +3,30 @@
     <!--<div>-->
     {{ $t('cultivatingSchemeQuery.studentType')}}:
     <el-select v-model="stuTypecode"  @change="getMajor" style="width: 15%" filterable placeholder="请选择">
-    <el-option
-    v-for="item in stuTypeList"
-    :key="item.value"
-    :label="$t(item.name)"
-    :value="item.value"
-    />
+      <el-option
+        v-for="item in stuTypeList"
+        :key="item.value"
+        :label="$t(item.name)"
+        :value="item.value"
+      />
     </el-select>
     {{ $t('cultivatingSchemeQuery.school') }}:
     <el-select  v-model="collegeType" @change="getMajor" style="width: 15%" filterable placeholder="请选择">
-    <el-option
-    v-for="item in collegeNameList"
-    :key="item.value"
-    :label="$t(item.name)"
-    :value="item.value"
-    />
+      <el-option
+        v-for="item in collegeNameList"
+        :key="item.value"
+        :label="$t(item.name)"
+        :value="item.value"
+      />
     </el-select>
     {{ $t('cultivatingSchemeQuery.major') }}:
     <el-select v-model="majorTypeCode"  style="width: 15%" filterable placeholder="请选择">
-    <el-option
-    v-for="item in majorList"
-    :key="item.value"
-    :label="$t(item.name)"
-    :value="item.value"
-    />
+      <el-option
+        v-for="item in majorList"
+        :key="item.value"
+        :label="$t(item.name)"
+        :value="item.value"
+      />
     </el-select>
     {{ $t('cultivatingSchemeQuery.year') }}:
     <el-input v-model="year" placeholder="请输入年份" style="width: 15%"></el-input>
@@ -38,93 +38,93 @@
       border
       fit
       highlight-current-row>
-    <el-table-column
-    :label="$t('projectParticipation.number')"
-    width="80">
-      <template slot-scope="scope">
-        {{ scope.$index+1 }}
-      </template>
-    </el-table-column>
-    <el-table-column
-    :label="$t('cultivatingSchemeQuery.cultivatingSchemeName')"
-    width="550">
-      <template slot-scope="scope">
-        {{ $t(scope.row.schemeName) }}
-      </template>
-    </el-table-column>
-    <el-table-column
-    :label="$t('cultivatingSchemeQuery.major1')"
-    width="230">
-      <template slot-scope="scope">
-        {{ $t(scope.row.majorName) }}
-      </template>
-    </el-table-column>
-    <el-table-column
-    :label="$t('cultivatingSchemeQuery.startTime')"
-    width="180">
-      <template slot-scope="scope">
-        {{ scope.row.startTimeStr }}
-      </template>
-    </el-table-column>
-    <el-table-column
-    :label="$t('cultivatingSchemeQuery.detail')"
-    >
-      <template slot-scope="scope">
-        <el-button @click="pushInfo(scope.row.schemeId )" type="text">详细</el-button>
-      </template>
-    </el-table-column>
+      <el-table-column
+        :label="$t('projectParticipation.number')"
+        width="80">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('cultivatingSchemeQuery.cultivatingSchemeName')"
+        width="550">
+        <template slot-scope="scope">
+          {{ $t(scope.row.schemeName) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('cultivatingSchemeQuery.major1')"
+        width="230">
+        <template slot-scope="scope">
+          {{ $t(scope.row.majorName) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('cultivatingSchemeQuery.startTime')"
+        width="180">
+        <template slot-scope="scope">
+          {{ scope.row.startTimeStr }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        :label="$t('cultivatingSchemeQuery.detail')"
+      >
+        <template slot-scope="scope">
+          <el-button @click="pushInfo(scope.row.schemeId , scope.row.majorName, scope.row.studentType )" type="text">详细</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 <script>
-import { cultivateSchemeShow , getMajorList ,getCultivateTableList } from '@/api/user'
-export default {
-  data() {
-    return {
-      tablelist: [],
-      cultivateFormList: '',
-      collegeNameList: [],
-      majorList: [],
-      stuTypeList: [],
-      stuTypecode: '',
-      year: '',
-      collegeType: '',
-      majorTypeCode: ''
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      cultivateSchemeShow().then(res => {
-        this.cultivateFormList = res.data.cultivateFormList
-        this.stuTypeList = res.data.stuTypeList
-        this.collegeNameList = res.data.collegeNameList
-      })
+  import { cultivateSchemeShow , getMajorList ,getCultivateTableList } from '@/api/user'
+  export default {
+    data() {
+      return {
+        tablelist: [],
+        cultivateFormList: '',
+        collegeNameList: [],
+        majorList: [],
+        stuTypeList: [],
+        stuTypecode: '',
+        year: '',
+        collegeType: '',
+        majorTypeCode: ''
+      }
     },
-    pushInfo(id){
-      this.$router.push({ name: 'showCultivate', params: { id }})
+    created() {
+      this.fetchData()
     },
-    getMajor(){
-      if(this.stuTypecode&&this.collegeType)
-      getMajorList({'stuTypecode': this.stuTypecode , 'collegeType': this.collegeType}).then(res => {
-          this.majorList = res.data.majorList
-         console.log(res)
-      }).catch(e => {
+    methods: {
+      fetchData() {
+        cultivateSchemeShow().then(res => {
+          this.cultivateFormList = res.data.cultivateFormList
+          this.stuTypeList = res.data.stuTypeList
+          this.collegeNameList = res.data.collegeNameList
+        })
+      },
+      pushInfo(id,majorName,studentType){
+        this.$router.push({ name: 'showCultivate', params: { id ,majorName,studentType}})
+      },
+      getMajor(){
+        if(this.stuTypecode&&this.collegeType)
+          getMajorList({'stuTypecode': this.stuTypecode , 'collegeType': this.collegeType}).then(res => {
+            this.majorList = res.data.majorList
+            console.log(res)
+          }).catch(e => {
 
-      })
-    },
-    getTableList(){
-      getCultivateTableList({'stuTypecode': this.stuTypecode , 'collegeType': this.collegeType ,'majorTypeCode': this.majorTypeCode ,'year': this.year }).then(res =>{
-        this.cultivateFormList = res.data.cultivateFormList
-        console.log(res)
-      }).catch(e => {
+          })
+      },
+      getTableList(){
+        getCultivateTableList({'stuTypecode': this.stuTypecode , 'collegeType': this.collegeType ,'majorTypeCode': this.majorTypeCode ,'year': this.year }).then(res =>{
+          this.cultivateFormList = res.data.cultivateFormList
+          console.log(res)
+        }).catch(e => {
 
-      })
+        })
+      }
     }
   }
-}
 </script>
 
 <style scoped>
--- src/views/pygl/pyfayxsgrjh/showCourse.vue
+++ src/views/pygl/pyfayxsgrjh/showCourse.vue
@@ -5,6 +5,13 @@
       border
       style="width: 100%">
       <el-table-column
+        :label="$t('projectParticipation.number')"
+        width="80">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+      <el-table-column
         prop="date"
         label="课程号"
         width="180">
@@ -14,14 +21,209 @@
       </el-table-column>
       <el-table-column
         prop="name"
-        label="姓名"
-        width="180">
+        label="课程名"
+        width="280">
+        <template slot-scope="scope">
+          {{  $t(scope.row.courseName) }}
+        </template>
       </el-table-column>
       <el-table-column
         prop="address"
-        label="地址">
+        label="开课学期"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{  $t(scope.row.term) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="总学时"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{ (scope.row.classHour) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="学分"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{ (scope.row.credit) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="任课教师"
+        width="200"
+      >
+        <template slot-scope="scope">
+          {{ (scope.row.teacherGroup) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="考核方式"
+      >
+        <template slot-scope="scope">
+          {{  $t(scope.row.examStyle) }}
+        </template>
       </el-table-column>
     </el-table>
+
+
+    <div>
+      <el-table
+      :data="optionalList"
+      border
+      style="width: 100%">
+      <el-table-column
+        :label="$t('projectParticipation.number')"
+        width="80">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="课程号"
+        width="180">
+        <template slot-scope="scope">
+          {{ (scope.row.courseNum) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="课程名"
+        width="280">
+        <template slot-scope="scope">
+          {{ $t(scope.row.courseName) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="开课学期"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{ $t(scope.row.term) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="总学时"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{ (scope.row.classHour) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="学分"
+        width="120"
+      >
+        <template slot-scope="scope">
+          {{ $t(scope.row.credit) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="任课教师"
+        width="200"
+      >
+        <template slot-scope="scope">
+          {{ (scope.row.teacherGroup) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="考核方式"
+      >
+        <template slot-scope="scope">
+          {{ $t(scope.row.examStyle) }}
+        </template>
+      </el-table-column>
+    </el-table>
+    </div>
+
+
+    <div>
+      <el-table
+        :data="buxiuList"
+        border
+        style="width: 100%">
+        <el-table-column
+          :label="$t('projectParticipation.number')"
+          width="80">
+          <template slot-scope="scope">
+            {{ scope.$index+1 }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="课程号"
+          width="180">
+          <template slot-scope="scope">
+            {{ (scope.row.courseNum) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="课程名"
+          width="280">
+          <template slot-scope="scope">
+            {{ $t(scope.row.courseName) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="开课学期"
+          width="120"
+        >
+          <template slot-scope="scope">
+            {{ $t(scope.row.term) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="总学时"
+          width="120"
+        >
+          <template slot-scope="scope">
+            {{ (scope.row.classHour) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="学分"
+          width="120"
+        >
+          <template slot-scope="scope">
+            {{ (scope.row.credit) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="任课教师"
+          width="200"
+        >
+          <template slot-scope="scope">
+            {{ (scope.row.teacherGroup) }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="考核方式"
+        >
+          <template slot-scope="scope">
+            {{ $t(scope.row.examStyle) }}
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 
 </template>
@@ -59,7 +261,9 @@ export default {
           this.post = post
         }
       }).then(res => {
-         this.requiredList = res.data.list1
+         this.requiredList = res.data.requiredList
+          this.optionalList = res.data.optionalList
+          this.buxiuList = res.data.buxiuList
       }
       )
     }
