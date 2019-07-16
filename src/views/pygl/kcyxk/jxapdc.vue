57fd48bb01ed37368df30559d6d02c383d5a36b0
wenxue11 <714876076@qq.com>
2019-07-16 10:51:25 +0800

    wx

--- src/views/pygl/kcyxk/jxapdc.vue
+++ src/views/pygl/kcyxk/jxapdc.vue
@@ -144,24 +144,7 @@ export default {
         this.termList = res.data.termList
         this.courseExportList = res.data.courseExportList
       })
-    },
-    // handleDownload() {
-    //   this.downloadLoading = true
-    //   import('@/vendor/Export2Excel').then(excel => {
-    //     const tHeader = ['Id', 'Title', 'Author', 'Readings', 'Date']
-    //     const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
-    //     const list = this.list
-    //     const data = this.formatJson(filterVal, list)
-    //     excel.export_json_to_excel({
-    //       header: tHeader,
-    //       data,
-    //       filename: this.filename,
-    //       autoWidth: this.autoWidth,
-    //       bookType: this.bookType
-    //     })
-    //     this.downloadLoading = false
-    //   })
-    // }
+    }
   }
 }
 </script>
