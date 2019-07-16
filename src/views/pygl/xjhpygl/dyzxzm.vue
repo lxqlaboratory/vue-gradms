e63106e5d37957642935d89743b1cfe10b6a6e56
wenxue11 <714876076@qq.com>
2019-07-16 15:08:02 +0800

    wx

--- src/views/pygl/xjhpygl/dyzxzm.vue
+++ src/views/pygl/xjhpygl/dyzxzm.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="container">
-    <a href="/gradms/api/student/printStudyProve" download="educationCertificate.pdf">{{$t('route.dyzxzm')}}</a>
+    <div style="color: #9b0d14">
+     <a href="/gradms/api/student/printStudyProve" download="educationCertificate.pdf" >{{$t('route.dyzxzm')}}</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-    }
-  }
-
 
 }
 </script>
